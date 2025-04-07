ee16af2ec6e24087ff41963f2adac613b2e61c0c
Nextixt <nextixt@gmail.com>
2025-04-07 08:29:47 +0300

    Update and rename app.py to HOWTOSTART.py

---
+++ HOWTOSTART.py
@@ -0,0 +1,4 @@
+'''Before running this program, make sure you have run the model.py file.'''
+from quadratic_solver import QuadraticSolver
+solver = QuadraticSolver()
+print(solver.solve(1, -3, 2))  # (2.0, 1.0)
