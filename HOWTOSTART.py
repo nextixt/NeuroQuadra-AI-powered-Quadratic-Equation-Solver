ebcf6122f3d7b7849ddd934f62a29f651bfa549c
Nextixt <nextixt@gmail.com>
2025-04-07 08:59:53 +0300

    Delete HOWTOSTART.py

--- HOWTOSTART.py
+++
@@ -1,4 +0,0 @@
-'''Before running this program, make sure you have run the model.py file.'''
-from quadratic_solver import QuadraticSolver
-solver = QuadraticSolver()
-print(solver.solve(1, -3, 2))  # (2.0, 1.0)
